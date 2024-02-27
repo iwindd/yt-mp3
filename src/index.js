52901ffa6e9481798c29fc8f7c6b6f6c50f3ee40
iwind <134185860+iwindd@users.noreply.github.com>
2024-02-27 19:46:48 +0700

    setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("RUNNING")
+const ytdl = require('ytdl-core');
+const ffmpegPath = require('ffmpeg-static');
+const ffmpeg = require('fluent-ffmpeg');
+const path = require('path');
+
+async function toMp3(videoUrl, outputDirectory) {
+  try {
+    const info = await ytdl.getInfo(videoUrl);
+    const title = info.videoDetails.title.replace(/[^\w\s]/gi, ''); // Remove special characters from the title
+    const videoReadableStream = ytdl(videoUrl, { filter: 'audioonly' });
+
+    ffmpeg.setFfmpegPath(ffmpegPath);
+    const outputPath = path.join(outputDirectory, `${title}.mp3`);
+    ffmpeg(videoReadableStream)
+      .format('mp3')
+      .save(outputPath)
+      .on('end', () => {
+        console.log('Conversion complete.');
+      });
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
